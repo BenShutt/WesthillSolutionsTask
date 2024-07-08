7e047daf1896c8bad6b711fe9fe927343cc4bc98
Apple <>
2024-07-08 16:04:55 +0000

    Republish sample code project.

--- RealtimeNumberReader/VisionViewController.swift
+++ RealtimeNumberReader/VisionViewController.swift
@@ -1,5 +1,5 @@
 /*
-See LICENSE folder for this sample’s licensing information.
+See the LICENSE.txt file for this sample’s licensing information.
 
 Abstract:
 The Vision view controller, which recognizes and displays bounding boxes around text.
