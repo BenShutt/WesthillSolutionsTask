2c1cfeb8664bf19daeb69f25bce047cb260da56a
Ben Shutt <shuttbenjamin@gmail.com>
2024-08-28 16:45:50 +0100

    Add MRZFeedback skeleton

---
+++ RealtimeNumberReader/MRZFeedback.swift
@@ -0,0 +1,37 @@
+//
+//  MRZFeedback.swift
+//  RealtimeNumberReader
+//
+//  Created by Ben Shutt on 28/08/2024.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import UIKit
+
+/// Feedback UI given to the user while they scan their MRZ.
+/// - Note: At this stage, the structure could be replaced by a `Result`.
+/// It is added to potentially support more cases in the future.
+enum MRZFeedback {
+
+    /// The scan was successful and returned the given text
+    case success(MRZ)
+
+    /// The scan was unsuccessful
+    case failure(String)
+
+    /// Overlay title text
+    var title: String {
+        switch self {
+        case let .success(mrz): mrz
+        case let .failure(string): string
+        }
+    }
+
+    /// Overlay text color
+    var textColor: UIColor {
+        switch self {
+        case let .success(mrz): .green
+        case let .failure(string): .red
+        }
+    }
+}
